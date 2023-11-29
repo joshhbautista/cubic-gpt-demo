0685c64f1e7995037d8623756027baf6d1b4a633
Joshua Bautista <hello@joshbautista.com>
2023-11-28 20:56:50 -0500

    Remove codeblock

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -2,7 +2,6 @@ import { Message } from 'ai';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import { cn } from '@/lib/utils';
-import { CodeBlock } from '@/components/ui/codeblock';
 import { IconOpenAI, IconUser } from '@/components/ui/icons';
 import { ChatMessageActions } from '@/components/chat-message-actions';
 import { MemoizedReactMarkdown } from '@/components/markdown';
@@ -34,36 +33,6 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
           components={{
             p({ children }) {
               return <p className="mb-2 last:mb-0">{children}</p>;
-            },
-            code({ node, inline, className, children, ...props }) {
-              if (children.length) {
-                if (children[0] == '▍') {
-                  return (
-                    <span className="mt-1 cursor-default animate-pulse">▍</span>
-                  );
-                }
-
-                children[0] = (children[0] as string).replace('`▍`', '▍');
-              }
-
-              const match = /language-(\w+)/.exec(className || '');
-
-              if (inline) {
-                return (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              }
-
-              return (
-                <CodeBlock
-                  key={Math.random()}
-                  language={(match && match[1]) || ''}
-                  value={String(children).replace(/\n$/, '')}
-                  {...props}
-                />
-              );
             }
           }}
         >
